b22d15411e7dba19451f107b27003953e1334df4
Asif Ahmed <abahmed@aconex.com>
2016-12-05 15:49:28 +0530

    Added MVC  test for controller with @WebMvcTest

---
+++ src/test/java/com/ekl/HelloControllerWebMvcITest.java
@@ -0,0 +1,33 @@
+package com.ekl;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+/**
+ * Created by abahmed on 5/12/16.
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloController.class)
+public class HelloControllerWebMvcITest {
+
+    @Autowired
+    private MockMvc mvc;
+
+
+    @Test
+    public void do_integration_test() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("gradle: Hello Spring Boot"));
+
+    }
+}
